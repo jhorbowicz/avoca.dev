2c8698e8a2be4dfa86fc6e7e0ad17cc4aede6e01
Jan Horbowicz <janhorbowicz@gmail.com>
2021-02-26 00:12:22 +0100

    🤌🏻 Go with the flow

---
+++ nuxt.config.js
@@ -0,0 +1,29 @@
+export default {
+  components: true,
+  buildModules: [
+    '@nuxtjs/tailwindcss',
+    ['@nuxtjs/google-fonts', {
+      families: {
+        Montserrat: [400, 600],
+        Cormorant: {
+          wght: [400, 600, 700],
+          ital: [400]
+        },
+      }
+    }]
+  ],
+  css: ['~/assets/css/global', '~/assets/css/tailwind'],
+  head: {
+    title: 'Internet retreat',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'avoca.dev',
+        name: 'avoca.dev',
+        content: 'under construction'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }]
+  }
+}
